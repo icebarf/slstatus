780c61e6c94887f36dc54f3f17c0a96895dc8a49
icebarf <sysgrammer@protonmail.com>
2023-05-19 19:17:53 +0530

    supress compiler warning by returning NULL

--- user_status.c
+++ user_status.c
@@ -153,6 +153,7 @@ static const char *state_to_str(const char state) {
 
   errno = EINVAL;
   die("state_to_str:");
+  return NULL; // supress control reaches end of non void function warning
 }
 
 static const char *battery_icon(const char *const perc) {
